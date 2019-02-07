961d5d5b32124f674ea963aeb9e71d93a0a50792
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-07 18:01:35 +0000

    Changed script for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main () {
-  puts("\"Programming is like building a multilingual puzzle\n");
+  puts("\"Programming is like building a multilingual puzzle");
   return(0);
 }
