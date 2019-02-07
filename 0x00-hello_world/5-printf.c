54c1f19785af8478dd234f05939a4e3ae1e799f0
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-07 21:25:16 +0000

    Changed formatting 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints a string
- * Description: prints an exact string to the command line followed by a new line
+ * Description: prints a string with printf, followed by a new line
  * Return: returns zero
  */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
