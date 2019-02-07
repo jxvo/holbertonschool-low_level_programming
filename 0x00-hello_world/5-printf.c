bc4f2b721e3a997b7bd5a6be538702f7c83e3855
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-07 21:23:15 +0000

    Created script 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ * Description: prints an exact string to the command line followed by a new line
+ * Return: returns zero
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
