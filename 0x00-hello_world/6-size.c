0cc7a1327bf5c4db2d7587862d469e99c38cf2f3
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-08 03:53:24 +0000

    Created script for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 	long longType;
 	long long int longlongintType;
 
-	printf("Size of a char: %ld byte(s)\n",sizeof(integerType);
-	printf("Size of an int: %ld byte(s)\n",sizeof(floatType);
+	printf("Size of a char: %ld byte(s)\n", sizeof(integerType);
+	printf("Size of an int: %ld byte(s)\n", sizeof(floatType);
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longlongintType);
 	printf("Size of a float: %ld byte(s)\n", sizeof(floatType);
 
