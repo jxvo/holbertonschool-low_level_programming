7ddd74343fa36d37a11b984d773c4fce5680eeef
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-12 21:57:32 +0000

    Re-compiled 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,20 +3,10 @@
 /**
  * _islower - checks for lowercase character
  * @c: The character to check
- * Return: ret value 0 or 1
+ * Return: returns 1 if lowercase
  */
 
 int _islower(int c)
 {
-	int  i;
-	int ret = 0;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (c == i)
-		{
-			ret = 1;
-		}
-	}
-	return (ret);
+	return (c >= 97 && c <= 122);
 }
