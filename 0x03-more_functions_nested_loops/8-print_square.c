8f030556f6c6403ff6f75b354c3aa442641e1d6c
Jonathan Vasquez <727@holberton.com>
2019-02-17 03:59:23 +0000

    Fixed int size checker

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -10,15 +10,17 @@ void print_square(int size)
 {
 	int x, y;
 
-	for (y = 0; y < size; y++)
+	if (size > 0)
 	{
-		if (size > 0)
+		for (y = 0; y < size; y++)
 		{
 			for (x = 0; x < size; x++)
 			{
 				_putchar('#');
 			}
-		}
 		_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
