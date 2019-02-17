452aafabf99d12531645e27da731685c04abaf7a
Jonathan Vasquez <727@holberton.com>
2019-02-17 04:13:26 +0000

    Initial compile

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100, with exceptions for multiples of 3 and 5
+ * Description: this proves I can code my way out of a wet paper bag
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	{
+		if (!(x % 3) && !(x % 5))
+			printf("FizzBuzz");
+		else if (!(x % 3))
+			printf("Fizz");
+		else if (!(x % 5))
+			printf("Buzz");
+		else
+			printf("%d", x);
+		if (x < 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
