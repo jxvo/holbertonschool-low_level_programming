59738a79b4e822269df11d05f2858507ceb2eaaa
Jonathan Vasquez <727@holberton.com>
2019-02-18 03:41:32 +0000

    Initial compile

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a quote
+ * Return: always 1;
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
