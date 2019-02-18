aea89740b26d3c9a18cc9c266faccf91028ff7eb
Jonathan Vasquez <727@holberton.com>
2019-02-18 05:11:44 +0000

    Recompiled 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
- * main - prints a quote
+ * main - prints a quote using write
  * Return: always 1;
  */
 
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
