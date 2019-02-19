d7929e620abd4dbc808147a775eeec7be904d49c
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-19 18:53:16 +0000

    Re-created holberton.h

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_HEADER
+#define HOLBERTON_HEADER
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
