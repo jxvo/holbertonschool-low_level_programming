f43d921768d81f43e9a5aa0b80a650c2381058e4
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-23 01:41:09 +0000

    Appeasing betty formatting

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  * _strncat - concatenates @src string for a max of @n bytes to string @dest
  * @dest: string to append to
  * @src: string to append
- * @n max bytes from @src
+ * @n: max bytes from @src
  * Return: concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
