b8d4b540e167552e0d4852cc1218771ec7bdc750
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-23 01:47:17 +0000

    Initial compile 2-strncpy.c

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - simple strncpy implementation
+ * @dest: string to replace
+ * @src: string to copy
+ * @n: number of bytes of @src to copy
+ * Return: modified @dest string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *sub;
+
+	for (sub = dest; 
+}
