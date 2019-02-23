a9372434128de3793e6ba58750fb9d0da4e84aff
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-23 01:59:12 +0000

    Initial compiled 2-strncpy.c

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -7,9 +7,13 @@
  * @n: number of bytes of @src to copy
  * Return: modified @dest string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	char *sub;
 
-	for (sub = dest; 
+	for (sub = dest; *sub && *src && n > 0; sub++, src++, n--)
+		*sub = *src;
+	for (; n > 0; n--, sub++)
+		*sub = '\0';
+	return (dest);
 }
