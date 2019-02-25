9ab7341891a51f2bd4a7dab2da91762d7ca75af6
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-25 00:21:32 +0000

    Created base files

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: number of elements to swap
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int *x;
+
+	for (x = a; *x; x++)
+		;
+	
+}
