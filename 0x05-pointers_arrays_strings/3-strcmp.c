4043d3ca46096ead46575803f4be4e497cb15bb0
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-25 00:14:56 +0000

    Added for loop

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 < *s2 || *s1 > *s2)
-		return (*s1 - *s2);
+	for (; *s1 && *s2; s1++, s2++)
+	{
+		if (*s1 < *s2 || *s1 > *s2)
+			return (*s1 - *s2);
+	}
 	return (0);
 }
