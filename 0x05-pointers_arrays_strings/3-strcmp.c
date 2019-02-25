4b42fcca36483e811fde5fa4175e4c60ec3c030b
Jonathan Vasquez <jxvo92@yahoo.com>
2019-02-25 00:03:13 +0000

    Initial compile 3-strcmp.c

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,14 @@
 #include "holberton.h"
 
 /**
- * _strcmp - compares two strings 
+ * _strcmp - compares the first ASCII values of two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: value < 0 if (@s1 < @s2), value > 0 if (@s1 > @s2), 0 if equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 < *s2 || *s1 > *s2)
+		return (*s1 - *s2);
+	return (0);
+}
