7960cfdd218db95d335121100b2d9465615d9577
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-01 19:41:25 +0000

    Created base files for 2-strchr.c

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to look for
+ * Return: pointer to first occurence, 'NULL' if not found
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s; 
+}
