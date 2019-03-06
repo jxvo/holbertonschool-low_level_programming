cd7fd61f71f2bd79dc7cdf13b22eeec9854faf12
Jonathan Vasquez <727@holberton.com>
2019-03-06 14:51:42 +0000

    Initial compile 0-create_array.c

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -9,18 +9,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size <= 0)
-		return (NULL);
-	else
-	{
-		char *arr;
-		int i;
+	char *arr;
+	unsigned int i;
 
-		arr = malloc(size * sizeof(char));
-		for (x = 0; i < size; i++)
-		{
-			arr[i] = c;
-		}
-		return (*arr);
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
 	}
+	return (arr);
 }
