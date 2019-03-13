f9638816c966a9b50aaf04ccc777b5c4c2582054
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-13 00:40:05 +0000

    Defined dog_t as new name for struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: float age (in human years)
  * @owner: *char name of human
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
