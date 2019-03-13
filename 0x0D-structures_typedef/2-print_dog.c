5e767d493175eaff9afda5f52ccfc7c3ead4fe86
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-13 00:21:51 +0000

    Initial compile 2-print_dog.c

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints doggo's information
+ * @d: pointer to doggo of struct dog
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	printf("Age: %f\n", d->age);
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)");
+}
