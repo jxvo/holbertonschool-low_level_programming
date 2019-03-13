092654f67c53391625929a02ca04d7695109aa25
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-13 18:22:30 +0000

    Initial compile 0-print_name.c

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name out of given string and case setting function
+ * @name: string name to print
+ * @f: case setting function (uppercase or lowercase)
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
