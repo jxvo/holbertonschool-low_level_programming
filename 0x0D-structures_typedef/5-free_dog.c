d856efd949e4ba48f1627d92062c24ad02002cf0
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-13 16:08:41 +0000

    Initial compile 5-free_dog.c

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees the memory allocated by doggos
+ * @d: pointer to doggo type dog_t of struct dog
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
