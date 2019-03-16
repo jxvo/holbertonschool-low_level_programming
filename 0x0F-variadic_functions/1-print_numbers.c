0f72e63ecf277fa68222a5206fe22839b101f684
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-16 19:06:01 +0000

    Initial compile 1-print_numbers.c

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints passed numbers, with passed separator, followed by \n
+ * @separator: what gets printed between each numbers, probably ", "
+ * @n: constant unsigned int numbers
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int x;
+
+	if (!separator)
+		return;
+	va_start(nums, n);
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(nums, unsigned int));
+		if (x < n - 1)
+			printf("%s", separator);
+	}
+	va_end(nums);
+	putchar('\n');
+}
