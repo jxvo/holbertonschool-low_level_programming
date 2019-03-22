e267380c7abe200e3da4355745b707784b4b5abd
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-22 03:53:53 +0000

    Initial compile 3-add_node_end.c

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node to the end of a list_t list
+ * @head: pointer to a pointer to list of struct type list_t
+ * @str: string to duplicate and create new node with
+ *
+ * Return: pointer to the new element, NULL if malloc failure
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *last_node;
+	unsigned int x;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str != '\0')
+	{
+		for (x = 0; new_node->str[x] != '\0'; x++)
+			;
+	}
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	new_node->len = x;
+	new_node->next = NULL;
+	last_node = *head;
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+	last_node->next = new_node;
+
+	return (last_node);
+}
---
+++ 0x11-singly_linked_lists/4-free-list.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees the allocated memory inside of a list_t list
+ * @head: pointer to list struct of type list_t
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+}
