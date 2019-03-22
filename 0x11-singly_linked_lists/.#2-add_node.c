db37dcf2f2ceb5a052a0a0215e2bd4e40b76433d
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-22 04:06:45 +0000

    Repositioned setting new_node->len before the NULL check for *head

---
+++ 0x11-singly_linked_lists/.#2-add_node.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.9603:1552759805
--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *last_node;
-	unsigned int x;
+	unsigned int  x;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -23,12 +23,12 @@ list_t *add_node_end(list_t **head, const char *str)
 		for (x = 0; new_node->str[x] != '\0'; x++)
 			;
 	}
+	new_node->len = x;
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	new_node->len = x;
 	new_node->next = NULL;
 	last_node = *head;
 	while (last_node->next != NULL)
