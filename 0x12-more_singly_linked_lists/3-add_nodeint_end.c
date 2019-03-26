158b70382502ae7eb37a6fa0cd029fc185a8c60d
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-26 14:03:51 +0000

    Initialized next pointer of new node as NULL

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,6 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_new;
 		return(new_new);
 	}
+	new_new->next = NULL;
 	last = *head;
 	while (last->next != NULL)
 		last = last->next;
