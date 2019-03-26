bbbebda8f33abc6ffcc40dbb26d1f552213feb70
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-26 13:56:30 +0000

    Removed initializing new node as NULL

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,7 +17,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new_new == NULL)
 		return (NULL);
 	new_new->n = n;
-	new_new->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new_new;
---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to first element in a listint_t list
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(head);
+	}
+}
