6ea7848e4dda0484e78259fd0282ad4a37360cc5
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-26 16:57:20 +0000

    Initial compile 5-free_listint2.c

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to pointer to first node in a listint_t list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
+}
