c256b0abb3c26a9e4825311becf711c2690855b7
Jonathan Vasquez <jxvo92@yahoo.com>
2019-03-26 16:09:01 +0000

    Betty formatting

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new_new;
-		return(new_new);
+		return (new_new);
 	}
 	last = *head;
 	while (last->next != NULL)
