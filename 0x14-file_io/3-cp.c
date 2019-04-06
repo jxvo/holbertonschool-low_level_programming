6fc48b3144db9f6580778ce87ecaaabde7d6bd44
Jonathan Vasquez <jxvo92@yahoo.com>
2019-04-06 08:44:37 +0000

    Initial compile 3-cp.c (Need to fix betty more than 40 line function)

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include "holberton.h"
 
 /**
@@ -9,18 +12,52 @@
  *
  * Return: 1 on success, exit with error code otherwise
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	char *f_to, *f_from;
 	char buffer[1024];
-	int fd_to, fd_from, bytes_read, bytes_wrote, close_to, close_from;
+	int fd_to, fd_from, bytes_read = 1024, bytes_wrote;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	f_from = argv[1];
-	f_to = argv[2];
-	
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (bytes_read == 1024)
+	{
+		bytes_read = read(fd_from, buffer, 1024);
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		bytes_wrote = write(fd_to, buffer, bytes_read);
+		if (bytes_wrote == -1 || bytes_wrote < bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	return (0);
 }
