480f3a16f75c3a9208cc8253c63411a26600265c
Jonathan Vasquez <jxvo92@yahoo.com>
2019-06-04 01:31:38 +0000

    prints a hash table

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: HashTable to print
+ *
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned int idx;
+
+	if (!ht || !ht->array)
+		return;
+	node = ht->array[0];
+	putchar('{');
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		node = ht->array[idx];
+		while (node)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			if (node->next != NULL)
+				printf(", ");
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
