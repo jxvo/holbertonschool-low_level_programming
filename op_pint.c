f55f167340ae7b5daf9798d672d83a5062e0d061
Jonathan Vasquez <727@holberton.com>
2019-07-01 06:22:55 +0000

    Update submodule

---
+++ op_pint.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+
+/**
+ * op_pint - prints the value at the top of the stack
+ * @head: double pointer to head node of stack
+ * @line_number: line number being interpreted from Monty file
+ *
+ * Return: nothuing, exit with code EXIT_FAILURE on failure
+ */
