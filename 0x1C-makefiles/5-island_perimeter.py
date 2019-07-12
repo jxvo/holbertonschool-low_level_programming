b4e696df59cd6fd73a92e7d7f1cc014c82907a68
Jonathan Vasquez <jxvo92@yahoo.com>
2019-07-12 02:13:42 +0000

    modified if statements

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,20 +8,16 @@ def island_perimeter(grid):
         for idx in range(len(grid[row])):
             if grid[row][idx] == 1:
                 """if 1 encountered check all sides for 0"""
-                if not grid[row - 1][idx] \
-                   or grid[row - 1][idx] == 0:
+                if grid[row - 1][idx] <= 0:
                     """check top index value"""
                     perimeter += 1
-                if not grid[row + 1][idx] \
-                   or grid[row + 1][idx] == 0:
+                if grid[row + 1][idx] <= 0:
                     """check bottom index value"""
                     perimeter += 1
-                if not grid[row][idx - 1] \
-                   or grid[row][idx - 1] == 0:
+                if grid[row][idx - 1] <= 0:
                     """check left index value"""
                     perimeter += 1
-                if not grid[row][idx + 1] \
-                   or grid[row][idx + 1] == 0:
+                if grid[row][idx + 1] <= 0:
                     """check right index value"""
                     perimeter += 1
     return perimeter
--- 0x1C-makefiles/5-main.py
+++ 0x1C-makefiles/5-main.py
@@ -5,11 +5,12 @@
 island_perimeter = __import__('5-island_perimeter').island_perimeter
 
 if __name__ == "__main__":
-    grid = [
+    """grid = [
         [0, 0, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 1, 1, 0, 0],
         [0, 0, 0, 0, 0, 0]
-    ]
+    ]"""
+    grid = [1]
     print(island_perimeter(grid))
