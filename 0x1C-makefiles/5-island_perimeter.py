a35dc10a8a59478968d154a39fbb4a819912f16a
Jonathan Vasquez <jxvo92@yahoo.com>
2019-07-12 01:55:37 +0000

    return the perimeter of an island grid (lists of ints)

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""island perimether function"""
+
+def island_perimeter(grid):
+    """returns the perimeter of an island grid from a list of integers"""
+    perimeter = 0
+    for row in range(len(grid)):
+        for idx in range(len(grid[row])):
+            if grid[row][idx] == 1:
+                """if 1 encountered check all sides for 0"""
+                if grid[row - 1][idx] is not None \
+                   and grid[row - 1][idx] == 0:
+                    """check top index value"""
+                    perimeter += 1
+                if grid[row + 1][idx] is not None \
+                   and grid[row + 1][idx] == 0:
+                    """check bottom index value"""
+                    perimeter += 1
+                if grid[row][idx - 1] is not None \
+                   and grid[row][idx - 1] == 0:
+                    """check left index value"""
+                    perimeter += 1
+                if grid[row][idx + 1] is not None \
+                   and grid[row][idx + 1] == 0:
+                    """check right index value"""
+                    perimeter += 1
+    return perimeter
