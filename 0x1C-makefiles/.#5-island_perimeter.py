750fec9257bb8ce287f1466d6e948dc6d396b3fe
Jonathan Vasquez <jxvo92@yahoo.com>
2019-07-12 02:51:30 +0000

    trying with all new if statements to see if it works

---
+++ 0x1C-makefiles/.#5-island_perimeter.py
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.14088:1562883384
--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,24 +8,32 @@ def island_perimeter(grid):
         for idx in range(len(grid[0])):
             if grid[row][idx] == 1:
                 """if 1 encountered check all sides for 0"""
-                if grid[row - 1][idx] == 0:
-                    """check top index value"""
+                top = row - 1
+                bottom = row + 1
+                left = idx - 1
+                right = idx + 1
+
+                """check top index value"""
+                if top < 0:
                     perimeter += 1
-                elif row - 1 < 0:
+                elif grid[row - 1][idx] != 1:
                     perimeter += 1
-                if grid[row + 1][idx] == 0:
-                    """check bottom index value"""
+
+                """check bottom index value"""
+                if bottom >= len(grid):
                     perimeter += 1
-                elif row + 1 >= len(grid):
+                elif grid[row + 1][idx] != 1:
                     perimeter += 1
-                if grid[row][idx - 1] == 0:
-                    """check left index value"""
+
+                """check left index value"""
+                if left < 0:
                     perimeter += 1
-                elif idx - 1 < 0:
+                elif grid[row][idx - 1] != 1:
                     perimeter += 1
-                if grid[row][idx + 1] == 0:
-                    """check right index value"""
+
+                """check right index value"""
+                if right >= len(grid[0]):
                     perimeter += 1
-                elif idx + 1 >= len(grid[0]):
+                elif grid[row][idx + 1] != 1:
                     perimeter += 1
     return perimeter
