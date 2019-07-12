468ac07aed7b2b2908234998be686dfedd1e55f0
Jonathan Vasquez <jxvo92@yahoo.com>
2019-07-12 02:33:56 +0000

    made some changes

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,23 +5,27 @@ def island_perimeter(grid):
     """returns the perimeter of an island grid from a list of integers"""
     perimeter = 0
     for row in range(len(grid)):
-        for idx in range(len(grid[row])):
+        for idx in range(len(grid[0])):
             if grid[row][idx] == 1:
                 """if 1 encountered check all sides for 0"""
-                if grid[row - 1][idx] == 0 \
-                   or row - 1 < 0:
+                if grid[row - 1][idx] == 0:
                     """check top index value"""
                     perimeter += 1
-                if grid[row + 1][idx] == 0 \
-                   or row + 1 >= len(grid):
+                elif row - 1 < 0:
+                    perimeter += 1
+                if grid[row + 1][idx] == 0:
                     """check bottom index value"""
                     perimeter += 1
-                if grid[row][idx - 1] == 0 \
-                   or idx - 1 < 0:
+                elif row + 1 >= len(grid):
+                    perimeter += 1
+                if grid[row][idx - 1] == 0:
                     """check left index value"""
                     perimeter += 1
-                if grid[row][idx + 1] == 0 \
-                   or idx + 1 >= len(grid[row]):
+                elif idx - 1 < 0:
+                    perimeter += 1
+                if grid[row][idx + 1] == 0:
                     """check right index value"""
+                    perimeter += 1
+                elif idx + 1 >= len(grid[0]):
                     perimeter += 1
     return perimeter
