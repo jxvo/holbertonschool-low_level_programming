2672907bce5f3f97bcab8501ca67bc1426d9a1f9
Jonathan Vasquez <jxvo92@yahoo.com>
2019-07-12 02:21:02 +0000

    modified if statements... again

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,16 +8,20 @@ def island_perimeter(grid):
         for idx in range(len(grid[row])):
             if grid[row][idx] == 1:
                 """if 1 encountered check all sides for 0"""
-                if grid[row - 1][idx] <= 0:
+                if grid[row - 1][idx] == 0 \
+                   or row - 1 < 0:
                     """check top index value"""
                     perimeter += 1
-                if grid[row + 1][idx] <= 0:
+                if grid[row + 1][idx] == 0 \
+                   or row + 1 >= len(grid):
                     """check bottom index value"""
                     perimeter += 1
-                if grid[row][idx - 1] <= 0:
+                if grid[row][idx - 1] == 0 \
+                   or idx - 1 < 0:
                     """check left index value"""
                     perimeter += 1
-                if grid[row][idx + 1] <= 0:
+                if grid[row][idx + 1] == 0 \
+                   or idx + 1 >= len(grid[row]):
                     """check right index value"""
                     perimeter += 1
     return perimeter
