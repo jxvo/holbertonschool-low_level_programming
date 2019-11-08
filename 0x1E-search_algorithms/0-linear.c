4770e2cab611689e4b9973c88f99f597777c7a49
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 08:27:07 +0000

    Betty formatting fixes: spaces to tabs

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,13 +10,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%i]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
